c4389717daf4b957bb69289c884b655170d20a7b
cheb <cheb@nuke.africa>
2021-04-19 13:34:00 +0200

    Copy the hashsets ONCE

--- src/bruteforce.cu
+++ src/bruteforce.cu
@@ -7,22 +7,21 @@
 //TODO check a bunch of hashes, AND SORT THEM BY SIZE!!! or maybe even build a pseudo binary-tree
 
 int main(){
+    //Copy hashsets to device
+    uint8_t *dh1;
+    uint32_t *dh2;
+    cudaMalloc(&dh1, 256);
+    cudaMalloc(&dh2, 4*256);
+    cudaMemcpy(dh1, hashSet1, 256, cudaMemcpyHostToDevice);
+    cudaMemcpy(dh2, hashSet2, 4*256, cudaMemcpyHostToDevice);
 
     uint32_t goal = 0x49541d5a;
-	int maxLen = 6;
+    int maxLen = 6;
 	for(int len=1;len<=maxLen;len++){
         //Checking 32 chars => max = 32^maxLen
 		uint64_t max=32;
 		for(int i=0;i<len;i++)
 			max*=32;
-
-		uint8_t *dh1;
-		uint32_t *dh2;
-		cudaMalloc(&dh1, 256);
-		cudaMalloc(&dh2, 4*256);
-	
-		cudaMemcpy(dh1, hashSet1, 256, cudaMemcpyHostToDevice);
-		cudaMemcpy(dh2, hashSet2, 4*256, cudaMemcpyHostToDevice);
 
 		uint64_t *d_result;
 		cudaMalloc(&d_result,sizeof(uint64_t));
@@ -32,8 +31,7 @@ int main(){
 		uint64_t result = 0;
 		cudaMemcpy(&result, d_result, sizeof(uint64_t), cudaMemcpyDeviceToHost);
 
-		cudaFree(dh1);
-		cudaFree(dh2);
+
 		if(result!=0){
 			char decoded[len];
 			decode(decoded,len,result);
@@ -41,5 +39,8 @@ int main(){
 			break;
 		}
 	}
+
+    cudaFree(dh1);
+    cudaFree(dh2);
 	return 0;
 }
